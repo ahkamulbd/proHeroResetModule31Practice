57006c48cc17350b15860b52992d911613642b57
ahkamulbd <ahkamulbd@gmail.com>
2023-03-21 11:44:57 +0600

    Template string practiced according to Module 31-4

---
+++ js/template.js
@@ -0,0 +1,12 @@
+const a = 20;
+const b = 30;
+const sum = `Sum of ${a} and ${b} are ${a + b}`;
+
+console.log(sum);
+
+const multiLines = `
+    I love Bangladesh
+    It is a beautiful Country
+    I got independence at 1971
+`;
+console.log(multiLines);
